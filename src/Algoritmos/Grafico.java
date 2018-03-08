887a97f9ce6ab87b21ee925587664e17e6ce361d
josef <jose.fl_@hotmail.com>
2018-03-08 10:53:39 -0600

    Paneles de Secante, Newton, clase grafico (vacia).

--- src/Algoritmos/Grafico.java
+++ src/Algoritmos/Grafico.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Algoritmos;
+import Paneles.PanelGrafico;
 
-/**
- *
- * @author josef
- */
-public class Grafico {
-    
+public class Grafico 
+{
+    public void grafica(String funcion)
+    {
+        //PanelGrafico.
+    }
 }
